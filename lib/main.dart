d20313a295f02b8887c75864374d13728fc6afe3
UssrFantom <dimkakrohin@mail.ru>
2021-10-09 18:33:10 +0300

    add style DancingScript

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'DancingScript'),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Adding Assets'),
@@ -17,7 +18,19 @@ class MyApp extends StatelessWidget {
               Image(
                 image: AssetImage('assets/images/backgroud.jpeg'),
               ),
-              Image.asset('assets/icons/icon.png')
+              Image.asset('assets/icons/icon.png'),
+              Positioned(
+                top: 120,
+                left: 125,
+                child: Text(
+                  "My custom font",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    //fontFamily: 'indieFlower',
+                  ),
+                ),
+              )
             ],
           ),
         ),
